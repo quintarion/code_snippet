aa0960220eb499fb5996840451cfae84c3aa09a7
Neto <quintarion@gmail.com>
2020-06-14 17:14:20 +0200

    start React app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          ReactJS code spinnet 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
